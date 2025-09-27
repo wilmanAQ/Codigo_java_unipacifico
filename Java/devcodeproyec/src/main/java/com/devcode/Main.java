f9306e1ec728d819c05667e5077b4fcab8870629
Wilman Andres Quiñonez Valencia <113271582+wilmanAQ@users.noreply.github.com>
2025-09-27 01:24:15 -0500

    Creacón de nuevo carro y motor

--- Java/devcodeproyec/src/main/java/com/devcode/Main.java
+++ Java/devcodeproyec/src/main/java/com/devcode/Main.java
@@ -5,7 +5,10 @@ public class Main {
         
         
         Motor motorV8 = new Motor("V8", 450);
+        Motor motorP8 = new Motor("P8", 550)
+        Carro auto = new Carro("Mazda", "Verder",2023, motroP8)
         Carro carro = new Carro("Mustang", "Vermelho", 2021, motorV8);
+        System.out.print("PRIMER CARRO")
         carro.acelerar();
         motorV8.ligar();
         motorV8.desligar();
